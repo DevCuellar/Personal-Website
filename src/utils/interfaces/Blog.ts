96eb070c9c927e5131f814aa542016785fbec92f
DevCuellar <contacto@davidcuellar.com>
2024-09-07 09:06:29 +0200

    Blog last entries and about page created.

---
+++ src/utils/interfaces/Blog.ts
@@ -0,0 +1,9 @@
+export interface BlogPost {
+    data: {
+        title: string;
+        description: string;
+        pubDate: Date;
+        updatedDate: Date;
+        heroImage: Date;
+    };
+}
